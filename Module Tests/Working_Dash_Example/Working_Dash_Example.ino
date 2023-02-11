dc7040e090c6e3c781b1b35438966e8b588304f9
ABashara <alexbashara2002@gmail.com>
2023-02-11 15:10:43 -0600

    Changes to dash files

--- Module Tests/Working_Dash_Example/Working_Dash_Example.ino
+++ Module Tests/Working_Dash_Example/Working_Dash_Example.ino
@@ -1,9 +1,4 @@
-// #include "Nextion.h"
-// #include "NexProgressBar.h"
 
-// NexText gear = NexText(0,1,"gear");
-// NexNumber rpm = NexNumber(0,1,"rpm");
-// NexProgressBar rpmBar = NexProgressBar(0,1,"rpmBar");
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 
@@ -14,31 +9,91 @@ char message[9];
 void setup() 
 {
     //Serial.begin(115200);
-    Serial1.begin(115200); //Nextion Serial Port
+    Serial2.begin(115200); //Nextion Serial Port
     Wire.begin();
-    uint8_t Sensor_Address = 0x5B; //Default 0x5A
+    uint8_t Sensor_Address = 0x5B; //Default 0x5A -- AB Sensor changed to 0x5B
     mlx.begin(Sensor_Address, &Wire);
 }
 
 void loop() 
 {
-    // gear.setText("P");
-    // rpm.setValue(rpm_val);
-    // rpm_val += 100;
-    // rpmBar.setValue(rpm_val/160);
-    // //delay(1);
-    // gear.setText("N");
-    // rpm.setValue(rpm_val);
-    // rpm_val += 100;
-    // rpmBar.setValue(rpm_val/160);
-    // //delay(1);
     uint8_t temp = mlx.readObjectTempC();
-    sprintf(message, "Temp: %d\"", temp);
-    Serial1.print("lastLap.txt=\"");
-    Serial1.print(message);     
-    Serial1.write(0xff);
-    Serial1.write(0xff);
-    Serial1.write(0xff);
+    int count = 10000;
+    
+    //rpm
+    sprintf(message, "%d\"", temp);
+    Serial2.print("rpm.txt=\"");
+    Serial2.print(message);     
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
     delay(1);
 
+    //gear
+    char Gear = '1';
+    sprintf(message, "%c\"", Gear);
+    Serial2.print("gear.txt=\"");
+    Serial2.print(message);     
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    delay(1);
+
+    //waterTemp
+    sprintf(message, "%d\"", temp);
+    Serial2.print("waterTemp.txt=\"");
+    Serial2.print(message);     
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    delay(1);
+
+    //oilPress
+    int oilP = 9;
+    sprintf(message, "%d\"", oilP);
+    Serial2.print("oilPress.txt=\"");
+    Serial2.print(message);     
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    delay(1);
+
+    //lap
+    int lap = 5;
+    sprintf(message, "%d\"", lap);
+    Serial2.print("lap.txt=\"");
+    Serial2.print(message);     
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    delay(1);
+
+    //lastLap
+    int min = 1;
+    int sec = 32;
+    int ms = 135;
+    sprintf(message, "%d.%d:%d\"", min, sec, ms);
+    Serial2.print("lastLap.txt=\"");
+    Serial2.print(message);     
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    delay(1);
+
+    //bestLap
+    sprintf(message, "%d.%d:%d\"", min, sec, ms);
+    Serial2.print("bestLap.txt=\"");
+    Serial2.print(message);     
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    delay(1);
+
+    //rpmBar
+    Serial2.print("rpmBar.val=");
+    Serial2.print(temp + 50);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    Serial2.write(0xff);
+    delay(1);
 }
