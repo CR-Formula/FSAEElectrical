caee72f078a3fea73a46d109c24137ad8820850e
Alex Bashara <alexbashara2002@gmail.com>
2024-03-02 22:02:30 -0600

    Cleaned up LoRa Code

--- Module Tests/Lora/Receiver/Receiver.ino
+++ Module Tests/Lora/Receiver/Receiver.ino
@@ -1,117 +1,48 @@
-// Feather9x_RX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (receiver)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Feather9x_TX
-
 #include <SPI.h>
 #include <RH_RF95.h>
 
-// First 3 here are boards w/radio BUILT-IN. Boards using FeatherWing follow.
-#if defined (__AVR_ATmega32U4__)  // Feather 32u4 w/Radio
+#if defined (__AVR_ATmega32U4__)  // Mega
   #define RFM95_CS    8
   #define RFM95_INT   7
   #define RFM95_RST   4
 
-#elif defined(ADAFRUIT_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0_EXPRESS) || defined(ARDUINO_SAMD_FEATHER_M0)  // Feather M0 w/Radio
-  #define RFM95_CS    8
-  #define RFM95_INT   3
-  #define RFM95_RST   4
-
-#elif defined(ARDUINO_ADAFRUIT_FEATHER_RP2040_RFM)  // Feather RP2040 w/Radio
-  #define RFM95_CS   16
-  #define RFM95_INT  21
-  #define RFM95_RST  17
-
-#elif defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+#elif defined (__AVR_ATmega328P__)  // Uno
   #define RFM95_CS    4  //
   #define RFM95_INT   3  //
   #define RFM95_RST   2  // "A"
 
-#elif defined(ESP8266)  // ESP8266 feather w/wing
-  #define RFM95_CS    2  // "E"
-  #define RFM95_INT  15  // "B"
-  #define RFM95_RST  16  // "D"
-
-#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2) || defined(ARDUINO_NRF52840_FEATHER) || defined(ARDUINO_NRF52840_FEATHER_SENSE)
-  #define RFM95_CS   10  // "B"
-  #define RFM95_INT   9  // "A"
-  #define RFM95_RST  11  // "C"
-
-#elif defined(ESP32)  // ESP32 feather w/wing
-  #define RFM95_CS   33  // "B"
-  #define RFM95_INT  27  // "A"
-  #define RFM95_RST  13
-
-#elif defined(ARDUINO_NRF52832_FEATHER)  // nRF52832 feather w/wing
-  #define RFM95_CS   11  // "B"
-  #define RFM95_INT  31  // "C"
-  #define RFM95_RST   7  // "A"
-
 #endif
 
-/* Some other possible setups include:
-
-// Feather 32u4:
-#define RFM95_CS   8
-#define RFM95_RST  4
-#define RFM95_INT  7
-
-// Feather M0:
-#define RFM95_CS   8
-#define RFM95_RST  4
-#define RFM95_INT  3
-
-// Arduino shield:
-#define RFM95_CS  10
-#define RFM95_RST  9
-#define RFM95_INT  7
-
-// Feather 32u4 w/wing:
-#define RFM95_RST 11  // "A"
-#define RFM95_CS  10  // "B"
-#define RFM95_INT  2  // "SDA" (only SDA/SCL/RX/TX have IRQ!)
-
-// Feather m0 w/wing:
-#define RFM95_RST 11  // "A"
-#define RFM95_CS  10  // "B"
-#define RFM95_INT  6  // "D"
-*/
-
-// Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
 typedef struct data_struct
 {
-  float RPM;        // RPM
-  float TPS;        // TPS
-  float FOT;        // Fuel Open Time
-  float IA;         // Ignition Angle
-  float Lam;        // Lambda
-  float AirT;       // Air Temp
-  float CoolT;      // Coolant Temp
-  float Speed;      // Vehicle Speed
-  float OilP;       // Oil Pressure
-  float FuelP;      // Fuel Pressure
-  float FLTemp;     // Front Left Brake Temp
-  float FRTemp;     // Front Right Brake Temp
-  float RLTemp;     // Rear Left Brake Temp
-  float RRTemp;     // Rear Right Brake Temp
-  float FRPot;      // Front Right Suspension Damper
-  float FLPot;      // Front Left Suspension Damper
-  float RRPot;      // Rear Right Suspension Damper
-  float RLPot;      // Rear Left Suspension Damper
-  float BrakeFront; // Front Brake Pressure
-  float BrakeRear;  // Rear Brake Pressure
-  float AccX;       // Accelerometer X Axis
-  float AccY;       // Accelerometer Y Axis
-  float AccZ;       // Accelerometer Z Axis
+  double RPM;        // RPM
+  double TPS;        // TPS
+  double FOT;        // Fuel Open Time
+  double IA;         // Ignition Angle
+  double Lam;        // Lambda
+  double AirT;       // Air Temp
+  double CoolT;      // Coolant Temp
+  double Speed;      // Vehicle Speed
+  double OilP;       // Oil Pressure
+  double FuelP;      // Fuel Pressure
+  double FLTemp;     // Front Left Brake Temp
+  double FRTemp;     // Front Right Brake Temp
+  double RLTemp;     // Rear Left Brake Temp
+  double RRTemp;     // Rear Right Brake Temp
+  double FRPot;      // Front Right Suspension Damper
+  double FLPot;      // Front Left Suspension Damper
+  double RRPot;      // Rear Right Suspension Damper
+  double RLPot;      // Rear Left Suspension Damper
+  double BrakeFront; // Front Brake Pressure
+  double BrakeRear;  // Rear Brake Pressure
+  double AccX;       // Accelerometer X Axis
+  double AccY;       // Accelerometer Y Axis
+  double AccZ;       // Accelerometer Z Axis
 } data_struct;
 data_struct telemetry;
 
-// Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 void setup() {
@@ -123,9 +54,8 @@ void setup() {
   while (!Serial) delay(1);
   delay(100);
 
-  Serial.println("Feather LoRa RX Test!");
+  Serial.println("LoRa RX Test!");
 
-  // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
@@ -133,71 +63,51 @@ void setup() {
 
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
-    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
   }
   Serial.println("LoRa radio init OK!");
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     while (1);
   }
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
 }
 
 void loop() {
-  if (rf95.available()) {
-    // Should be a message for us now
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  if (rf95.available()) { // Check for new message
+    uint8_t buf[sizeof(telemetry)]; // buffer for message
     uint8_t len = sizeof(buf);
 
     if (rf95.recv(buf, &len)) {
-      memcpy(&telemetry, buf, sizeof(telemetry));
-      // Serial.print("RSSI: ");
-      // Serial.println(rf95.lastRssi(), DEC);
-
-    // Print out the data struct
-    Serial.println("Data:");
-    Serial.println(telemetry.RPM);
-    Serial.println(telemetry.TPS);
-    Serial.println(telemetry.FOT);
-    Serial.println(telemetry.IA);
-    Serial.println(telemetry.Lam);
-    Serial.println(telemetry.AirT);
-    Serial.println(telemetry.CoolT);
-    Serial.println(telemetry.Speed);
-    Serial.println(telemetry.OilP);
-    Serial.println(telemetry.FuelP);
-    Serial.println(telemetry.FLTemp);
-    Serial.println(telemetry.FRTemp);
-    Serial.println(telemetry.RLTemp);
-    Serial.println(telemetry.RRTemp);
-    Serial.println(telemetry.FRPot);
-    Serial.println(telemetry.FLPot);
-    Serial.println(telemetry.RRPot);
-    Serial.println(telemetry.RLPot);
-    Serial.println(telemetry.BrakeFront);
-    Serial.println(telemetry.BrakeRear);
-    Serial.println(telemetry.AccX);
-    Serial.println(telemetry.AccY);
-    Serial.println(telemetry.AccZ);
-    Serial.println("End of Data");
-
-      // Send a reply
-      uint8_t data[] = "1";
-      rf95.send(data, sizeof(data));
-      rf95.waitPacketSent();
-      Serial.println("Sent a reply");
-    } else {
-      Serial.println("Receive failed");
+      memcpy(&telemetry, buf, sizeof(telemetry)); // Copy the message into the data struct
+      // Print out the data struct
+      Serial.println("Data:");
+      Serial.println(telemetry.RPM);
+      Serial.println(telemetry.TPS);
+      Serial.println(telemetry.FOT);
+      Serial.println(telemetry.IA);
+      Serial.println(telemetry.Lam);
+      Serial.println(telemetry.AirT);
+      Serial.println(telemetry.CoolT);
+      Serial.println(telemetry.Speed);
+      Serial.println(telemetry.OilP);
+      Serial.println(telemetry.FuelP);
+      Serial.println(telemetry.FLTemp);
+      Serial.println(telemetry.FRTemp);
+      Serial.println(telemetry.RLTemp);
+      Serial.println(telemetry.RRTemp);
+      Serial.println(telemetry.FRPot);
+      Serial.println(telemetry.FLPot);
+      Serial.println(telemetry.RRPot);
+      Serial.println(telemetry.RLPot);
+      Serial.println(telemetry.BrakeFront);
+      Serial.println(telemetry.BrakeRear);
+      Serial.println(telemetry.AccX);
+      Serial.println(telemetry.AccY);
+      Serial.println(telemetry.AccZ);
+      Serial.println("End of Data");
     }
   }
 }
--- Module Tests/Lora/Sender/Sender.ino
+++ Module Tests/Lora/Sender/Sender.ino
@@ -1,116 +1,47 @@
-// Feather9x_TX
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messaging client (transmitter)
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example Feather9x_RX
-
 #include <SPI.h>
 #include <RH_RF95.h>
 
-// First 3 here are boards w/radio BUILT-IN. Boards using FeatherWing follow.
-#if defined (__AVR_ATmega32U4__)  // Feather 32u4 w/Radio
+#if defined (__AVR_ATmega32U4__)  // Mega
   #define RFM95_CS    8
   #define RFM95_INT   7
   #define RFM95_RST   4
 
-#elif defined(ADAFRUIT_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0_EXPRESS) || defined(ARDUINO_SAMD_FEATHER_M0)  // Feather M0 w/Radio
-  #define RFM95_CS    8
-  #define RFM95_INT   3
-  #define RFM95_RST   4
-
-#elif defined(ARDUINO_ADAFRUIT_FEATHER_RP2040_RFM)  // Feather RP2040 w/Radio
-  #define RFM95_CS   16
-  #define RFM95_INT  21
-  #define RFM95_RST  17
-
-#elif defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+#elif defined (__AVR_ATmega328P__)  // Uno
   #define RFM95_CS    4  //
   #define RFM95_INT   3  //
   #define RFM95_RST   2  // "A"
 
-#elif defined(ESP8266)  // ESP8266 feather w/wing
-  #define RFM95_CS    2  // "E"
-  #define RFM95_INT  15  // "B"
-  #define RFM95_RST  16  // "D"
-
-#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2) || defined(ARDUINO_NRF52840_FEATHER) || defined(ARDUINO_NRF52840_FEATHER_SENSE)
-  #define RFM95_CS   10  // "B"
-  #define RFM95_INT   9  // "A"
-  #define RFM95_RST  11  // "C"
-
-#elif defined(ESP32)  // ESP32 feather w/wing
-  #define RFM95_CS   33  // "B"
-  #define RFM95_INT  27  // "A"
-  #define RFM95_RST  13
-
-#elif defined(ARDUINO_NRF52832_FEATHER)  // nRF52832 feather w/wing
-  #define RFM95_CS   11  // "B"
-  #define RFM95_INT  31  // "C"
-  #define RFM95_RST   7  // "A"
-
 #endif
 
-/* Some other possible setups include:
-
-// Feather 32u4:
-#define RFM95_CS   8
-#define RFM95_RST  4
-#define RFM95_INT  7
-
-// Feather M0:
-#define RFM95_CS   8
-#define RFM95_RST  4
-#define RFM95_INT  3
-
-// Arduino shield:
-#define RFM95_CS  10
-#define RFM95_RST  9
-#define RFM95_INT  7
-
-// Feather 32u4 w/wing:
-#define RFM95_RST 11  // "A"
-#define RFM95_CS  10  // "B"
-#define RFM95_INT  2  // "SDA" (only SDA/SCL/RX/TX have IRQ!)
-
-// Feather m0 w/wing:
-#define RFM95_RST 11  // "A"
-#define RFM95_CS  10  // "B"
-#define RFM95_INT  6  // "D"
-*/
-
-// Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
-// Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 typedef struct data_struct
 {
-  float RPM;        // RPM
-  float TPS;        // TPS
-  float FOT;        // Fuel Open Time
-  float IA;         // Ignition Angle
-  float Lam;        // Lambda
-  float AirT;       // Air Temp
-  float CoolT;      // Coolant Temp
-  float Speed;      // Vehicle Speed
-  float OilP;       // Oil Pressure
-  float FuelP;      // Fuel Pressure
-  float FLTemp;     // Front Left Brake Temp
-  float FRTemp;     // Front Right Brake Temp
-  float RLTemp;     // Rear Left Brake Temp
-  float RRTemp;     // Rear Right Brake Temp
-  float FRPot;      // Front Right Suspension Damper
-  float FLPot;      // Front Left Suspension Damper
-  float RRPot;      // Rear Right Suspension Damper
-  float RLPot;      // Rear Left Suspension Damper
-  float BrakeFront; // Front Brake Pressure
-  float BrakeRear;  // Rear Brake Pressure
-  float AccX;       // Accelerometer X Axis
-  float AccY;       // Accelerometer Y Axis
-  float AccZ;       // Accelerometer Z Axis
+  double RPM;        // RPM
+  double TPS;        // TPS
+  double FOT;        // Fuel Open Time
+  double IA;         // Ignition Angle
+  double Lam;        // Lambda
+  double AirT;       // Air Temp
+  double CoolT;      // Coolant Temp
+  double Speed;      // Vehicle Speed
+  double OilP;       // Oil Pressure
+  double FuelP;      // Fuel Pressure
+  double FLTemp;     // Front Left Brake Temp
+  double FRTemp;     // Front Right Brake Temp
+  double RLTemp;     // Rear Left Brake Temp
+  double RRTemp;     // Rear Right Brake Temp
+  double FRPot;      // Front Right Suspension Damper
+  double FLPot;      // Front Left Suspension Damper
+  double RRPot;      // Rear Right Suspension Damper
+  double RLPot;      // Rear Left Suspension Damper
+  double BrakeFront; // Front Brake Pressure
+  double BrakeRear;  // Rear Brake Pressure
+  double AccX;       // Accelerometer X Axis
+  double AccY;       // Accelerometer Y Axis
+  double AccZ;       // Accelerometer Z Axis
 } data_struct;
 data_struct telemetry;
 
@@ -122,9 +53,8 @@ void setup() {
   while (!Serial) delay(1);
   delay(100);
 
-  Serial.println("Feather LoRa TX Test!");
+  Serial.println("LoRa TX Test!");
 
-  // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
@@ -132,23 +62,15 @@ void setup() {
 
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
-    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
   }
   Serial.println("LoRa radio init OK!");
 
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     while (1);
   }
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
 
   telemetry.RPM = 1;
@@ -176,51 +98,37 @@ void setup() {
   telemetry.AccZ = 23;
 }
 
-// int16_t packetnum = 0;  // packet counter, we increment per xmission
-
 void loop() {
-  Serial.println(telemetry.RPM);
-  Serial.println(sizeof(float));
-  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
-  Serial.println("Transmitting..."); // Send a message to rf95_server
-
-  char radiopacket[RH_RF95_MAX_MESSAGE_LEN];
-
-  memcpy(radiopacket, &telemetry, sizeof(telemetry));
-
-  Serial.print("Sending ");
-  Serial.println(radiopacket);
-  // for (int i = 0; i < 23; i++) {
-  //   radiopacket[i] = *(&telemetry.RPM + i * sizeof(float));
-  //   Serial.print(radiopacket[i]);
-  //   Serial.print(", ");
-  // }
-  // radiopacket[19] = 0;
+  char radiopacket[sizeof(telemetry)]; // Buffer for data transmission
+  memcpy(radiopacket, &telemetry, sizeof(telemetry)); // Copy data from struct
 
   Serial.println("Sending...");
-  delay(10);
   rf95.send((uint8_t *)radiopacket, sizeof(radiopacket));
 
   Serial.println("Waiting for packet to complete...");
-  delay(10);
   rf95.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
 
-  Serial.println("Waiting for reply...");
-  if (rf95.waitAvailableTimeout(1000)) {
-    // Should be a reply message for us now
-    if (rf95.recv(buf, &len)) {
-      Serial.print("Got reply: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
-    } else {
-      Serial.println("Receive failed");
-    }
-  } else {
-    Serial.println("No reply, is there a listener around?");
-  }
-
+  telemetry.RPM += 1;
+  telemetry.TPS += 1;
+  telemetry.FOT += 1;
+  telemetry.IA += 1;
+  telemetry.Lam += 1;
+  telemetry.AirT += 1;
+  telemetry.CoolT += 1;
+  telemetry.Speed += 1;
+  telemetry.OilP += 1;
+  telemetry.FuelP += 1;
+  telemetry.FLTemp += 1;
+  telemetry.FRTemp += 1;
+  telemetry.RLTemp += 1;
+  telemetry.RRTemp += 1;
+  telemetry.FRPot += 1;
+  telemetry.FLPot += 1;
+  telemetry.RRPot += 1;
+  telemetry.RLPot += 1;
+  telemetry.BrakeFront += 1;
+  telemetry.BrakeRear += 1;
+  telemetry.AccX += 1;
+  telemetry.AccY += 1;
+  telemetry.AccZ += 1;
 }
